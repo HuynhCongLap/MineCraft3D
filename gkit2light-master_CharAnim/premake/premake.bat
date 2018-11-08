934db5f67fd91f680b965988768776140cf52117
HuynhCongLap <huynh.lap.co@gmail.com>
2018-11-08 01:27:53 +0100

    delete

--- gkit2light-master_CharAnim/premake/premake.bat
+++
@@ -1,9 +0,0 @@
-
-cd ..
-.\premake\premake4.exe --os=windows gmake
-.\premake\premake4.exe --os=windows codeblocks
-
-.\premake\premake5.exe --os=windows vs2013
-.\premake\premake5.exe --os=windows vs2015
-
-pause
